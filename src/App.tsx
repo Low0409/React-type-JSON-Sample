13b48c68b38b9884abd1ce0749ef2bf2c682fd9b
Low0409 <kitagawa0409@icloud.com>
2022-01-06 07:09:06 +0900

    df

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import InputForm from './components/InputForm';
-
+import ItemList from './components/itemList/ItemList';
 function App() {
   
   return (
     <div className="App">
+      <h2>useEffect</h2>
         <Counter/>
-        <InputForm/>
+        <ItemList/>
     </div>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,10 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Counter:React.VFC = () => {
     const [count,setCount] = useState(10)
+    useEffect(()=>{
+        console.log("useEffect発生");
+    },[count])
     return (
         <button onClick={()=>setCount(count+1)}>
             {count}
